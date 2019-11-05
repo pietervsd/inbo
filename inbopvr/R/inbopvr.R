107f876b2e28900e53619a81193e57e163731942
Pieter <pieter.verschelde@inbo.be>
2019-11-05 11:04:12 +0100

    usethis::create_package

---
+++ inbopvr/R/inbopvr.R
@@ -0,0 +1,11 @@
+
+#' \code{inbopvr} package
+#'
+#' @docType package
+#' @name inbopvr
+#' @importFrom dplyr %>%
+#' @importFrom purrr %||%
+NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if (getRversion() >= "2.15.1")  utils::globalVariables(c("."))
